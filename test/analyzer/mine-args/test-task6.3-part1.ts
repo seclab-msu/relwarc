2ea48b670c877b0d11f94f8120ba9e280db72f7d
pyatakovns <pyatakovns@seclab.cs.msu.ru>
2020-06-24 19:38:16 +0300

    deleted test_task6_3 and transferred files
    transfered everything to the folders named mine-args mine-deps and data
    
    see #1126

--- test/analyzer/mine-args/test-task6.3-part1.ts
+++ test/analyzer/mine-args/test-task6.3-part1.ts
@@ -1,38 +1,52 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const analyzer_1 = require("../../../src/analyzer/analyzer");
-const fs_1 = require("fs");
-const unknown_1 = require("../../../src/analyzer/types/unknown");
-function readSrc(path) {
-    return fs_1.readFileSync(path, "utf8");
+import { Analyzer, SinkCall } from "../../../src/analyzer/analyzer";
+import { readFileSync } from "fs";
+
+import {
+    UNKNOWN,
+    UNKNOWN_FUNCTION,
+    UNKNOWN_FROM_FUNCTION,
+} from "../../../src/analyzer/types/unknown";
+
+function readSrc(path: string): string {
+    return readFileSync(path, "utf8");
 }
+
 function readCheckFromFile(path) {
     const check = JSON.parse(readSrc(path), function (k, v) {
         if (v === "UNKNOWN") {
-            return unknown_1.UNKNOWN;
+            return UNKNOWN;
         }
         if (v === "FROM_FUNCTION_CALL") {
-            return unknown_1.UNKNOWN;
+            return UNKNOWN;
         }
         return v;
     });
     return check;
 }
+
 function check(argsFromAnalyzer, argsFromChecker) {
     for (let i = 0; i < argsFromChecker.length; i++) {
-        expect(argsFromAnalyzer).toContain(argsFromChecker[i]);
+        expect(argsFromAnalyzer).toContain(argsFromChecker[i] as SinkCall);
     }
 }
-function makeAndRunSimple(script, url = "http://example.com/") {
-    const analyzer = new analyzer_1.Analyzer();
+
+function makeAndRunSimple(
+    script: string,
+    url = "http://example.com/"
+): Analyzer {
+    const analyzer = new Analyzer();
     analyzer.addScript(script);
     analyzer.mineArgsForDEPCalls(url);
     return analyzer;
 }
+
 describe("Analyzer finding args of DEP sinks (from task 6.3)", () => {
     it("sample 1", () => {
         const test = 1;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
         expect(analyzer.results[0]).toEqual({
             funcName: "$.ajax",
@@ -43,17 +57,21 @@ describe("Analyzer finding args of DEP sinks (from task 6.3)", () => {
                     type: "GET",
                     cache: false,
                     data: {
-                        q: unknown_1.UNKNOWN,
+                        q: UNKNOWN,
                     },
-                    success: unknown_1.UNKNOWN,
-                    error: unknown_1.UNKNOWN,
+                    success: UNKNOWN,
+                    error: UNKNOWN,
                 },
             ],
-        });
+        } as SinkCall);
     });
+
     it("sample 2", () => {
         const test = 2;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(1);
         expect(analyzer.results[0]).toEqual({
             funcName: "$.ajax",
@@ -65,7 +83,7 @@ describe("Analyzer finding args of DEP sinks (from task 6.3)", () => {
                     },
                 },
             ],
-        });
+        } as SinkCall);
         expect(analyzer.results[1]).toEqual({
             funcName: "fetch",
             args: [
@@ -78,20 +96,28 @@ describe("Analyzer finding args of DEP sinks (from task 6.3)", () => {
                     },
                 },
             ],
-        });
+        } as SinkCall);
     });
+
     it("sample 3", () => {
         const test = 3;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
         expect(analyzer.results[0]).toEqual({
             funcName: "$http.get",
             args: ["/Umbraco/EuroNCAP/Widgets/GetTweets/17131"],
-        });
+        } as SinkCall);
     });
+
     it("sample 4", () => {
         const test = 4;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
         expect(analyzer.results[0]).toEqual({
             funcName: "fetch",
@@ -102,14 +128,19 @@ describe("Analyzer finding args of DEP sinks (from task 6.3)", () => {
                     headers: {
                         "Content-Type": "application/x-www-form-urlencoded",
                     },
-                    body: "action=wikiPageView&url=http%3A%2F%2Fjs-training.seclab%2Fjs-dep%2Ffunc-args%2Fsamples%2Fcomputed%2F4.html",
+                    body:
+                        "action=wikiPageView&url=http%3A%2F%2Fjs-training.seclab%2Fjs-dep%2Ffunc-args%2Fsamples%2Fcomputed%2F4.html",
                 },
             ],
-        });
+        } as SinkCall);
     });
+
     it("sample 5", () => {
         const test = 5;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
         expect(analyzer.results[0]).toEqual({
             funcName: "fetch",
@@ -124,25 +155,34 @@ describe("Analyzer finding args of DEP sinks (from task 6.3)", () => {
                     credentials: "include",
                 },
             ],
-        });
+        } as SinkCall);
     });
+
     it("sample 6", () => {
         const test = 6;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
         expect(analyzer.results[0]).toEqual({
             funcName: "$http",
             args: [
                 {
                     method: "GET",
-                    url: "https://www.site24x7.com/benchmarks/app?vertical=UNKNOWN&daySeparator=UNKNOWN",
+                    url:
+                        "https://www.site24x7.com/benchmarks/app?vertical=UNKNOWN&daySeparator=UNKNOWN",
                 },
             ],
-        });
+        } as SinkCall);
     });
+
     it("sample 7", () => {
         const test = 7;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
         expect(analyzer.results[0]).toEqual({
             funcName: "fetch",
@@ -153,34 +193,60 @@ describe("Analyzer finding args of DEP sinks (from task 6.3)", () => {
                     credentials: "include",
                     headers: {
                         "content-type": "application/json",
-                        "X-RequestDigest": unknown_1.UNKNOWN,
+                        "X-RequestDigest": UNKNOWN,
                     },
-                    body: '{"formId":"83e3b0f2-1aea-4e88-a9f7-70c399316d2e","formState":"UNKNOWN","fieldValues":"UNKNOWN","reCaptchaResponse":"UNKNOWN","files":"UNKNOWN"}',
+                    body:
+                        '{"formId":"83e3b0f2-1aea-4e88-a9f7-70c399316d2e","formState":"UNKNOWN","fieldValues":"UNKNOWN","reCaptchaResponse":"UNKNOWN","files":"UNKNOWN"}',
                 },
             ],
-        });
+        } as SinkCall);
     });
+
     it("sample 18", () => {
         const test = 18;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(3);
-        check(analyzer.results, readCheckFromFile(__dirname + `/data/check/${test}_args.json`));
+        check(
+            analyzer.results,
+            readCheckFromFile(__dirname + `/../data/check/${test}_args.json`)
+        );
     });
+
     xit("sample 19 (coming soon)", () => {
         const test = 19;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(3);
-        check(analyzer.results, readCheckFromFile(__dirname + `/data/check/${test}_args.json`));
+        check(
+            analyzer.results,
+            readCheckFromFile(__dirname + `/../data/check/${test}_args.json`)
+        );
     });
+
     it("sample 20", () => {
         const test = 20;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(3);
-        check(analyzer.results, readCheckFromFile(__dirname + `/data/check/${test}_args.json`));
+        check(
+            analyzer.results,
+            readCheckFromFile(__dirname + `/../data/check/${test}_args.json`)
+        );
     });
+
     it("sample 21", () => {
         const test = 21;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
         expect(analyzer.results[0]).toEqual({
             funcName: "fetch",
@@ -191,9 +257,10 @@ describe("Analyzer finding args of DEP sinks (from task 6.3)", () => {
                     headers: {
                         "Content-Type": "application/json",
                     },
-                    body: '{"$type":"runner:error","message":"UNKNOWN","stack":"UNKNOWN"}',
+                    body:
+                        '{"$type":"runner:error","message":"UNKNOWN","stack":"UNKNOWN"}',
                 },
             ],
-        });
+        } as SinkCall);
     });
 });
--- test/analyzer/mine-deps/test-task6.3-part1.ts
+++ test/analyzer/mine-deps/test-task6.3-part1.ts
@@ -1,17 +1,33 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const analyzer_1 = require("../../../src/analyzer/analyzer");
-const fs_1 = require("fs");
-const unknown_1 = require("../../../src/analyzer/types/unknown");
-function readSrc(path) {
-    return fs_1.readFileSync(path, "utf8");
+import { Analyzer } from "../../../src/analyzer/analyzer";
+import { HAR } from "../../../src/analyzer/hars.js";
+import { readFileSync } from "fs";
+import {
+    UNKNOWN,
+    UNKNOWN_FUNCTION,
+    UNKNOWN_FROM_FUNCTION,
+} from "../../../src/analyzer/types/unknown";
+
+function readSrc(path): any {
+    return readFileSync(path, "utf8");
 }
+
 function readCheckFromFile(path) {
     const check = JSON.parse(readSrc(path));
     return check;
 }
-function makeSimpleHar(dep) {
-    const check = {
+
+function makeSimpleHar(dep): any {
+    interface NewHar {
+        url: any;
+        method: any;
+        httpVersion: any;
+        headers: any;
+        queryString: any;
+        bodySize: any;
+        postData?: any;
+    }
+
+    const check: NewHar = {
         url: dep.url,
         method: dep.method,
         httpVersion: dep.httpVersion,
@@ -23,14 +39,20 @@ function makeSimpleHar(dep) {
     if (check.postData == null) {
         delete check.postData;
     }
+
     return check;
 }
-function makeAndRunSimple(script, url = "http://example.com/") {
-    const analyzer = new analyzer_1.Analyzer();
+
+function makeAndRunSimple(
+    script: string,
+    url = "http://example.com/"
+): Analyzer {
+    const analyzer = new Analyzer();
     analyzer.addScript(script);
     analyzer.analyze(url);
     return analyzer;
 }
+
 function convertToSet(deps) {
     deps.forEach((dep) => {
         dep.headers = new Set(dep.headers);
@@ -38,8 +60,9 @@ function convertToSet(deps) {
     });
     return deps;
 }
+
 function checker(depsFromAnalyzer, depsFromChecker) {
-    let hars = [];
+    let hars: any[] = [];
     depsFromAnalyzer.forEach((dep) => {
         hars.push(makeSimpleHar(dep));
     });
@@ -47,15 +70,21 @@ function checker(depsFromAnalyzer, depsFromChecker) {
         expect(hars).toContain(depsFromChecker[i]);
     }
 }
+
 describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
     it("sample 1", () => {
-        const analyzer = makeAndRunSimple(readSrc(__dirname + "/data/1.js"), "http://js-training.seclab/js-dep/func-args/samples/computed/1.html");
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + "/../data/1.js"),
+            "http://js-training.seclab/js-dep/func-args/samples/computed/1.html"
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
+
         const dep = analyzer.hars;
         const check = [
             {
                 httpVersion: "HTTP/1.1",
-                url: "http://js-training.seclab/jp/eltex/xsp/ajax/custom/AjaxSuggestBean.json?q=UNKNOWN",
+                url:
+                    "http://js-training.seclab/jp/eltex/xsp/ajax/custom/AjaxSuggestBean.json?q=UNKNOWN",
                 queryString: [
                     {
                         name: "q",
@@ -72,11 +101,17 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
                 method: "GET",
             },
         ];
+
         checker(dep, convertToSet(check));
     });
+
     it("sample 2", () => {
-        const analyzer = makeAndRunSimple(readSrc(__dirname + "/data/2.js"), "http://js-training.seclab/js-dep/func-args/samples/computed/2.html");
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + "/../data/2.js"),
+            "http://js-training.seclab/js-dep/func-args/samples/computed/2.html"
+        );
         expect(analyzer.results.length).toBeGreaterThan(1);
+
         const dep = analyzer.hars;
         const check = [
             {
@@ -124,9 +159,14 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
         ];
         checker(dep, convertToSet(check));
     });
+
     it("sample 3", () => {
-        const analyzer = makeAndRunSimple(readSrc(__dirname + "/data/3.js"), "http://js-training.seclab/js-dep/func-args/samples/computed/3.html");
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + "/../data/3.js"),
+            "http://js-training.seclab/js-dep/func-args/samples/computed/3.html"
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
+
         const dep = analyzer.hars;
         const check = [
             {
@@ -140,14 +180,20 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
                         name: "Host",
                     },
                 ],
-                url: "http://js-training.seclab/Umbraco/EuroNCAP/Widgets/GetTweets/17131",
+                url:
+                    "http://js-training.seclab/Umbraco/EuroNCAP/Widgets/GetTweets/17131",
             },
         ];
         checker(dep, convertToSet(check));
     });
+
     it("sample 4", () => {
-        const analyzer = makeAndRunSimple(readSrc(__dirname + "/data/4.js"), "http://js-training.seclab/js-dep/func-args/samples/computed/4.html");
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + "/../data/4.js"),
+            "http://js-training.seclab/js-dep/func-args/samples/computed/4.html"
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
+
         const dep = analyzer.hars;
         const check = [
             {
@@ -178,19 +224,26 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
                             name: "action",
                         },
                         {
-                            value: "http%3A%2F%2Fjs-training.seclab%2Fjs-dep%2Ffunc-args%2Fsamples%2Fcomputed%2F4.html",
+                            value:
+                                "http%3A%2F%2Fjs-training.seclab%2Fjs-dep%2Ffunc-args%2Fsamples%2Fcomputed%2F4.html",
                             name: "url",
                         },
                     ],
-                    text: "action=wikiPageView&url=http%3A%2F%2Fjs-training.seclab%2Fjs-dep%2Ffunc-args%2Fsamples%2Fcomputed%2F4.html",
+                    text:
+                        "action=wikiPageView&url=http%3A%2F%2Fjs-training.seclab%2Fjs-dep%2Ffunc-args%2Fsamples%2Fcomputed%2F4.html",
                 },
             },
         ];
         checker(dep, convertToSet(check));
     });
+
     it("sample 5", () => {
-        const analyzer = makeAndRunSimple(readSrc(__dirname + "/data/5.js"), "http://js-training.seclab/js-dep/func-args/samples/computed/5.html");
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + "/../data/5.js"),
+            "http://js-training.seclab/js-dep/func-args/samples/computed/5.html"
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
+
         const dep = analyzer.hars;
         const check = [
             {
@@ -221,14 +274,20 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
         ];
         checker(dep, convertToSet(check));
     });
+
     it("sample 6", () => {
-        const analyzer = makeAndRunSimple(readSrc(__dirname + "/data/6.js"), "http://js-training.seclab/js-dep/func-args/samples/computed/6.html");
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + "/../data/6.js"),
+            "http://js-training.seclab/js-dep/func-args/samples/computed/6.html"
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
+
         const dep = analyzer.hars;
         const check = [
             {
                 bodySize: 0,
-                url: "https://www.site24x7.com/benchmarks/app?vertical=UNKNOWN&daySeparator=UNKNOWN",
+                url:
+                    "https://www.site24x7.com/benchmarks/app?vertical=UNKNOWN&daySeparator=UNKNOWN",
                 headers: [
                     {
                         name: "Host",
@@ -251,15 +310,22 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
         ];
         checker(dep, convertToSet(check));
     });
+
     it("sample 7", () => {
-        const analyzer = makeAndRunSimple(readSrc(__dirname + "/data/7.js"), "http://js-training.seclab/js-dep/func-args/samples/computed/7.html");
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + "/../data/7.js"),
+            "http://js-training.seclab/js-dep/func-args/samples/computed/7.html"
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
+
         const dep = analyzer.hars;
         const check = [
             {
-                url: "http://js-training.seclab/ODVA/_vti_bin/OID.SharePoint.FormBuilder/submissions.svc/",
+                url:
+                    "http://js-training.seclab/ODVA/_vti_bin/OID.SharePoint.FormBuilder/submissions.svc/",
                 postData: {
-                    text: '{"formId":"83e3b0f2-1aea-4e88-a9f7-70c399316d2e","formState":"UNKNOWN","fieldValues":"UNKNOWN","reCaptchaResponse":"UNKNOWN","files":"UNKNOWN"}',
+                    text:
+                        '{"formId":"83e3b0f2-1aea-4e88-a9f7-70c399316d2e","formState":"UNKNOWN","fieldValues":"UNKNOWN","reCaptchaResponse":"UNKNOWN","files":"UNKNOWN"}',
                     mimeType: "application/json",
                 },
                 headers: [
@@ -273,7 +339,7 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
                     },
                     {
                         name: "X-RequestDigest",
-                        value: unknown_1.UNKNOWN,
+                        value: UNKNOWN,
                     },
                     {
                         name: "Content-Length",
@@ -289,40 +355,70 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
         // expect(makeSimpleHar(dep[0])).toEqual(convertToSet(check)[0]);
         checker(dep, convertToSet(check));
     });
+
     it("sample 18", () => {
         const test = 18;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(3);
+
         const dep = analyzer.hars;
-        const check = readCheckFromFile(__dirname + `/data/check/${test}_hars.json`);
+        const check = readCheckFromFile(
+            __dirname + `/../data/check/${test}_hars.json`
+        );
+
         checker(dep, convertToSet(check));
     });
+
     xit("sample 19 (coming soon)", () => {
         const test = 19;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(12);
+
         const dep = analyzer.hars;
-        const check = readCheckFromFile(__dirname + `/data/check/${test}_hars.json`);
+        const check = readCheckFromFile(
+            __dirname + `/../data/check/${test}_hars.json`
+        );
+
         // expect(makeSimpleHar(dep[2])).toEqual(convertToSet(check)[2]);
         // checker(dep, convertToSet(check));
     });
+
     it("sample 20", () => {
         const test = 20;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(2);
+
         const dep = analyzer.hars;
-        const check = readCheckFromFile(__dirname + `/data/check/${test}_hars.json`);
+        const check = readCheckFromFile(
+            __dirname + `/../data/check/${test}_hars.json`
+        );
+
         checker(dep, convertToSet(check));
     });
+
     it("sample 21", () => {
         const test = 21;
-        const analyzer = makeAndRunSimple(readSrc(__dirname + `/data/${test}.js`), `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`);
+        const analyzer = makeAndRunSimple(
+            readSrc(__dirname + `/../data/${test}.js`),
+            `http://js-training.seclab/js-dep/func-args/samples/computed/${test}.html`
+        );
         expect(analyzer.results.length).toBeGreaterThan(0);
+
         const dep = analyzer.hars;
         const check = [
             {
                 bodySize: 62,
-                url: "https://report.seznamzpravy.cz.test.js-training.seclab/report/custom",
+                url:
+                    "https://report.seznamzpravy.cz.test.js-training.seclab/report/custom",
                 httpVersion: "HTTP/1.1",
                 headers: [
                     {
@@ -342,7 +438,8 @@ describe("Analyzer mining HARs for JS DEPs (from task 6.3)", () => {
                 queryString: [],
                 postData: {
                     mimeType: "application/json",
-                    text: '{"$type":"runner:error","message":"UNKNOWN","stack":"UNKNOWN"}',
+                    text:
+                        '{"$type":"runner:error","message":"UNKNOWN","stack":"UNKNOWN"}',
                 },
             },
         ];
