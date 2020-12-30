8b30d5f384e24976570340bb59dca33b491790be
asterite <asterite@seclab.cs.msu.ru>
2020-12-30 14:24:00 +0300

    Add stub uncommenter

---
+++ src/analyzer/uncommenter.ts
@@ -0,0 +1,14 @@
+import {
+    Comment,
+    File as AST
+} from '@babel/types';
+
+export function combineComments(
+    splitComments: Comment[] | undefined | null
+): Comment[] {
+    return [];
+}
+
+export function parseComments(comments: Comment[]): AST[] {
+    return [];
+}
