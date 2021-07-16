007ddfad65379a8cf667f003dd2b792ba479b109
Denis Mironov <mirond@seclab.cs.msu.ru>
2021-07-16 12:15:41 +0300

    Add tests for POST forms in html

--- test/analyzer/page/test-html-deps.ts
+++ test/analyzer/page/test-html-deps.ts
@@ -132,5 +132,108 @@ describe('Analyzing HTML DEPs', () => {
             "bodySize": 0
         }));
     });
+
+    it('simple form POST request', async () => {
+        const dpa = new DynamicPageAnalyzer();
+        const url = testWS.getFullURL('/test-html-dep6.html');
+
+        await dpa.run(url);
+
+        expect(dpa.htmlDEPs.length).toBeGreaterThan(0);
+
+        const hars = dpa.htmlDEPs.map(JSONObjectFromHAR);
+
+        expect(hars).toContain(jasmine.objectContaining({
+            "method": "POST",
+            "url": testWS.getFullURL('/application/eihoS4/interface/zee5Ii/handle'),
+            "httpVersion": "HTTP/1.1",
+            "queryString": [],
+            "bodySize": 7,
+            "postData": {
+                "text": "ae9ieC=",
+                "mimeType": "application/x-www-form-urlencoded",
+                "params": [
+                    {
+                        "name": "ae9ieC",
+                        "value": ""
+                    }
+                ]
+            }
+        }));
+    });
+
+    it('form POST request with multiple params', async () => {
+        const dpa = new DynamicPageAnalyzer();
+        const url = testWS.getFullURL('/test-html-dep7.html');
+
+        await dpa.run(url);
+
+        expect(dpa.htmlDEPs.length).toBeGreaterThan(0);
+
+        const hars = dpa.htmlDEPs.map(JSONObjectFromHAR);
+
+        expect(hars).toContain(jasmine.objectContaining({
+            "method": "POST",
+            "url": testWS.getFullURL('/application/asdfds/interface/dsads/handle'),
+            "httpVersion": "HTTP/1.1",
+            "queryString": [],
+            "bodySize": 31,
+            "postData": {
+                "text": "password=&tag=default&username=",
+                "mimeType": "application/x-www-form-urlencoded",
+                "params": [
+                    {
+                        "name": "password",
+                        "value": ""
+                    },
+                    {
+                        "name": "tag",
+                        "value": "default"
+                    },
+                    {
+                        "name": "username",
+                        "value": ""
+                    }
+                ]
+            }
+        }));
+    });
+
+    it('multipart form POST request', async () => {
+        const dpa = new DynamicPageAnalyzer();
+        const url = testWS.getFullURL('/test-html-dep8.html');
+
+        await dpa.run(url);
+
+        expect(dpa.htmlDEPs.length).toBeGreaterThan(0);
+
+        const hars = dpa.htmlDEPs.map(JSONObjectFromHAR);
+
+        expect(hars).toContain(jasmine.objectContaining({
+            "method": "POST",
+            "url": testWS.getFullURL('/application/m1nk1l/interface/1l23nl/handle'),
+            "httpVersion": "HTTP/1.1",
+            "queryString": [],
+            "bodySize": 42,
+            "postData": {
+                "text": null,
+                "mimeType": "multipart/form-data",
+                "params": [
+                    {
+                        "name": "password",
+                        "value": "random"
+                    },
+                    {
+                        "name": "tag",
+                        "value": "default"
+                    },
+                    {
+                        "name": "username",
+                        "value": "admin"
+                    }
+                ]
+            }
+        }));
+    });
 });
 
