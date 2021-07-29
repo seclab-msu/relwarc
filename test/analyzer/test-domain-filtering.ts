85a21debbc7d3d283d5d371260837f2125ff3444
asterite <asterite@seclab.cs.msu.ru>
2021-07-29 15:45:13 +0300

    Refactor: group domain filtering tests by mode

--- test/analyzer/test-domain-filtering.ts
+++ test/analyzer/test-domain-filtering.ts
@@ -1,117 +1,130 @@
 import { filterByDomain, DomainFilteringMode } from '../../src/analyzer/domain-filtering';
 
 describe('Unit-tests for domain-filtering module', () => {
-    it('filterByDomain Any mode: subdomain', () => {
-        const url = 'http://example.subdomain.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Any);
-        expect(res).toBeTrue();
-    });
+    describe('filterByDomain Any mode', () => {
+        const mode = DomainFilteringMode.Any;
 
-    it('filterByDomain Any mode: different domains', () => {
-        const url = 'http://example.subdomain.com';
-        const baseUrl = 'http://another-domain.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Any);
-        expect(res).toBeTrue();
-    });
+        it('subdomain', () => {
+            const url = 'http://example.subdomain.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
 
-    it('filterByDomain Any mode: same domains', () => {
-        const url = 'http://example.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Any);
-        expect(res).toBeTrue();
-    });
+        it('different domains', () => {
+            const url = 'http://example.subdomain.com';
+            const baseUrl = 'http://another-domain.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
 
-    it('filterByDomain Subdomain mode: subdomain', () => {
-        const url = 'http://subdomain.example.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Subdomain);
-        expect(res).toBeTrue();
+        it('same domains', () => {
+            const url = 'http://example.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
     });
+    describe('filterByDomain Subdomain mode', () => {
+        const mode = DomainFilteringMode.Subdomain;
 
-    it('filterByDomain Subdomain mode: different domains', () => {
-        const url = 'http://subdomain-example.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Subdomain);
-        expect(res).toBeFalse();
-    });
+        it('subdomain', () => {
+            const url = 'http://subdomain.example.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
 
-    it('filterByDomain Subdomain mode: different domains', () => {
-        const url = 'http://example.subdomain.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Subdomain);
-        expect(res).toBeFalse();
-    });
+        it('different domains', () => {
+            const url = 'http://subdomain-example.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeFalse();
+        });
 
-    it('filterByDomain Subdomain mode: same domain', () => {
-        const url = 'http://subdomain.example.com';
-        const baseUrl = 'http://subdomain.example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Subdomain);
-        expect(res).toBeTrue();
-    });
+        it('different domains', () => {
+            const url = 'http://example.subdomain.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeFalse();
+        });
 
-    it('filterByDomain Same mode: same domain', () => {
-        const url = 'http://example.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Same);
-        expect(res).toBeTrue();
+        it('same domain', () => {
+            const url = 'http://subdomain.example.com';
+            const baseUrl = 'http://subdomain.example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
     });
+    describe('filterByDomain Same mode', () => {
+        const mode = DomainFilteringMode.Same;
 
-    it('filterByDomain Same mode: different domain', () => {
-        const url = 'http://example.com';
-        const baseUrl = 'http://anotherexample.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Same);
-        expect(res).toBeFalse();
-    });
+        it('same domain', () => {
+            const url = 'http://example.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
 
-    it('filterByDomain Same mode: different domain', () => {
-        const url = 'http://example.com';
-        const baseUrl = 'http://anotherexample.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Same);
-        expect(res).toBeFalse();
-    });
+        it('different domain', () => {
+            const url = 'http://example.com';
+            const baseUrl = 'http://anotherexample.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeFalse();
+        });
 
-    it('filterByDomain Same mode: subdomain', () => {
-        const url = 'http://api.example.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.Same);
-        expect(res).toBeFalse();
-    });
+        it('different domain', () => {
+            const url = 'http://example.com';
+            const baseUrl = 'http://anotherexample.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeFalse();
+        });
 
-    it('filterByDomain SecondLevel mode: same domain', () => {
-        const url = 'http://example.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.SecondLevel);
-        expect(res).toBeTrue();
+        it('subdomain', () => {
+            const url = 'http://api.example.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeFalse();
+        });
     });
-    it('filterByDomain SecondLevel mode: subdomain', () => {
-        const url = 'http://api.example.com';
-        const baseUrl = 'http://example.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.SecondLevel);
-        expect(res).toBeTrue();
-    });
-    it('filterByDomain SecondLevel mode: completely different domains', () => {
-        const url = 'http://example.subdomain.com';
-        const baseUrl = 'http://another-domain.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.SecondLevel);
-        expect(res).toBeFalse();
-    });
-    it('filterByDomain SecondLevel mode: different subdomains', () => {
-        const url = 'http://a.domain.com';
-        const baseUrl = 'http://b.domain.com';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.SecondLevel);
-        expect(res).toBeTrue();
-    });
-    it('filterByDomain SecondLevel mode: different subdomains with longer tld', () => {
-        const url = 'http://a.domain.co.uk';
-        const baseUrl = 'http://b.domain.co.uk';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.SecondLevel);
-        expect(res).toBeTrue();
-    });
-    it('filterByDomain SecondLevel mode: different domains with longer tld', () => {
-        const url = 'http://a.onedomain.co.uk';
-        const baseUrl = 'http://b.otherdomain.co.uk';
-        const res = filterByDomain(url, baseUrl, DomainFilteringMode.SecondLevel);
-        expect(res).toBeFalse();
+    describe('filterByDomain SecondLevel mode', () => {
+        const mode = DomainFilteringMode.SecondLevel;
+
+        it('same domain', () => {
+            const url = 'http://example.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
+        it('subdomain', () => {
+            const url = 'http://api.example.com';
+            const baseUrl = 'http://example.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
+        it('completely different domains', () => {
+            const url = 'http://example.subdomain.com';
+            const baseUrl = 'http://another-domain.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeFalse();
+        });
+        it('different subdomains', () => {
+            const url = 'http://a.domain.com';
+            const baseUrl = 'http://b.domain.com';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
+        it('different subdomains with longer tld', () => {
+            const url = 'http://a.domain.co.uk';
+            const baseUrl = 'http://b.domain.co.uk';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeTrue();
+        });
+        it('different domains with longer tld', () => {
+            const url = 'http://a.onedomain.co.uk';
+            const baseUrl = 'http://b.otherdomain.co.uk';
+            const res = filterByDomain(url, baseUrl, mode);
+            expect(res).toBeFalse();
+        });
     });
 });
