16033103fa0eb5295f158a2ca23c083df028ef2e
Dmitriy Nazarov <dmitry.nazarov@solidwall.io>
2021-08-12 16:23:12 +0300

    Change running script

--- run-analyzer-tar.sh
+++ run-analyzer-tar.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-exec unshare -mnr -- $SCRIPT_DIR/tarserver/tarserver $@
+. $SCRIPT_DIR/src/run-tar.sh $SCRIPT_DIR/tarserver/tarserver $@
