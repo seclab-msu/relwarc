c25e66fc37388c13bf0640cf03ddf7ed044babf0
asterite <asterite@seclab.cs.msu.ru>
2021-11-22 04:00:07 +0300

    Move deepCopyObject() to a separate module

--- src/analyzer/types/value-set.ts
+++ src/analyzer/types/value-set.ts
@@ -6,36 +6,7 @@ import { FunctionValue } from './function';
 
 import { log } from '../logging';
 
-function deepCopyObject(o: Value): Value {
-    if (typeof o === 'function') {
-        throw new Error('deepCopyObject: unexpected function');
-    }
-    if (typeof o !== 'object' || o === null) {
-        return o;
-    }
-    if (isUnknown(o)) {
-        return o;
-    }
-    if (Array.isArray(o)) {
-        return o.map(el => deepCopyObject(el));
-    }
-    if (o instanceof ValueSet) {
-        const newVS = new ValueSet();
-        newVS.values = new Set(o.values);
-        return newVS;
-    }
-    if (o instanceof FormDataModel) {
-        return o.copy();
-    }
-    if (o instanceof FunctionValue || o instanceof RegExp || o instanceof URL) {
-        return o;
-    }
-    const result = {};
-    for (const k of Object.keys(o)) {
-        result[k] = deepCopyObject(o[k]);
-    }
-    return result;
-}
+import { deepCopyObject } from './deep-copy';
 
 type TravCb = (
     ob: Array<Value> | Record<string, Value> | FormDataModel,
