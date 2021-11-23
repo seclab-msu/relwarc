f021562f11c1219c713053a5bd0ea58192aa5f17
asterite <asterite@seclab.cs.msu.ru>
2021-11-23 20:14:18 +0300

    Add tests for deep-copy.ts

---
+++ test/analyzer/test-deep-copy.ts
@@ -0,0 +1,29 @@
+import { deepCopyObject } from '../../src/analyzer/types/deep-copy';
+
+describe('Test deepCopyObject function', () => {
+    it('smoke test', () => {
+        const TEST_OBJECT = {
+            x: 123,
+            y: 'test',
+            z: ['a', 100, null]
+        };
+
+        const copy = deepCopyObject(TEST_OBJECT) as object;
+
+        expect(copy).not.toBe(TEST_OBJECT);
+
+        expect(copy).toEqual(TEST_OBJECT);
+    });
+
+    it('cyclic value', () => {
+        const ob: Record<string, object> = {};
+
+        ob.recur = ob;
+
+        const copy = deepCopyObject(ob) as object;
+
+        expect(copy).not.toBe(ob);
+
+        expect(copy).toEqual(ob);
+    });
+});
